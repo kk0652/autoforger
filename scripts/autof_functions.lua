dffd52becd2b9da9dc4ab12751385885fc0eccdf
&$13 <74501714+not-bill@users.noreply.github.com>
2022-11-02 08:12:18 +0100

    sort of weapon

--- scripts/autof_functions.lua
+++ scripts/autof_functions.lua
@@ -1,23 +1,64 @@
 local fns = {}
 
-local delaysTable = { -- should it be here? Though it needs some sorting if so
-    Dart        = 170,
-    Staff       = 135,
-    Book        = 335,
-    Hammer      = 270,
-    Spear       = 270,
-    SpiralSpear = 270,
-    Gauntlet    = 170,
-    Pocketwatch = 335,
-    Lucy        = 335,
-    Sword       = 270,
-    Spatula     = 270,
-    SoulStaff   = 270,
-    Bomb        = 270,
+local defaultDelays = { -- should it be here? Though it needs some sorting if so
+    Hand        = 295,
+    Dart        = 195,
+    Staff       = 160,
+    Book        = 360,
+    Hammer      = 295,
+    Spear       = 295,
+    SpiralSpear = 295,
+    Axe         = 295,
+    Bomb        = 295,
+    Sword       = 295,
+    -- ReForged
+    Slingshot   = 395,-- should it be higher? 420??
+    SoulStaff   = 295,
+    SpiceBomb   = 295,
+    Spatula     = 295,
+    Gauntlet    = 200,
+    Pocketwatch = 360,
+    -- Hallowed Forge
+    Graveaxe    = 160,
 }
 
 local function GetWeapon(weapon)
-    -- later, just don't touch it
+    if weapon ~= nil then
+        if weapon:HasTag('blowdart') then
+            return 'Dart'
+        elseif weapon.prefab:find('staff') then
+            return 'Staff'
+        elaeif weapon:HasTag('book') then
+            return 'Book'
+        elseif weapon.prefab:find('hammer') then
+            return 'Hammer'
+        elseif weapon.prefab:find('pithpike') then
+            return 'Spear'
+        elseif weapon.prefab:find('spiralspear') then
+            return 'SpiralSpear'
+       elseif weapon.prefab:find('lucy') then
+           return 'Axe'
+       elseif weapon.prefab:find('firebomb') then
+           return 'Bomb'
+       elseif weapon.prefab:find('blacksmithsedge') then
+           return 'Sword'
+       -- ReForged
+       elseif weapon.prefab:find('slingshot') then
+           return 'Slingshot'
+       elseif weapon:HasTag('soulstealer') then
+           return 'SoulStaff'
+        end
+       elseif weapon.prefab:find('spice_bomb') then
+           return 'SpiceBomb'
+       elseif weapon.prefab:find('spatula') then
+           return 'Spatula'
+       elseif weapon.prefab:find('gauntlet') then
+           return 'Gauntlet'
+       elseif weapon.prefab:find('pocketwatch') then
+           return 'Pocketwatch'
+       -- Hallowed Forge
+    end
+    return 'Hand'
 end
 
 local function GetScreenName() -- does it need to be part of fns?
@@ -27,7 +68,7 @@ local function GetScreenName() -- does it need to be part of fns?
 end
 
 function fns.GetDefaultDelay(weapon)
-    return delaysTable(GetWeapon(weapon))
+    return defaultDelays(GetWeapon(weapon))
 end
 
 function fns.IsTargetValid(target)
